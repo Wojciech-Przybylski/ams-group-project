540eeb83bbc1421e5af0b9ff5e4c187cae3c54fc
James E. Bryer <jamesbryer098@gmail.com>
2023-09-05 14:10:56 +0100

    Added sign up route

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,24 @@
 from flask import render_template, request, redirect, url_for, session
 from sqlalchemy import desc
-from application import app, db
+from application import app, db, bcrypt
+from application.models import User, Movie, Comment, Genre, MovieGenre, Actor, MovieActor, Director, MovieDirector, SignUpForm
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html', title='Home')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        print('validated')
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            password=bcrypt.generate_password_hash(form.password.data),
+            phone=form.phone.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('/signup.html', title='Sign Up', form=form)
