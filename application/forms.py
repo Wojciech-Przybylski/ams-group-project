bb5ad0bc2f7d1ea8b2d485acd75e0b68565e26d9
James E. Bryer <jamesbryer098@gmail.com>
2023-09-05 14:10:14 +0100

    Changed form

--- application/forms.py
+++ application/forms.py
@@ -3,25 +3,14 @@ from wtforms import StringField, SubmitField, SelectField, DateField, IntegerFie
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 from application.models import CheckAdmin, BannedChars, User, Category, Product, CheckPostcode
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
-    email = StringField('Email', validators=[DataRequired(), Email(), BannedChars()])
-    password = PasswordField('Password', validators=[DataRequired(), BannedChars()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password'), BannedChars()])
+class SignUpForm(FlaskForm):
+    name = StringField('Full Name', validators=[DataRequired(), Length(min=2, max=30), BannedChars(), CheckAdmin(message="Name cannot be 'admin'.")])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=30)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=30), BannedChars()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=2, max=30), BannedChars(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        admin = CheckAdmin(username.data)
-        if user:
-            raise ValidationError('That username is taken. Please choose another one.')
-        elif admin:
-            raise ValidationError('That username is not allowed. Please choose another one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
-        admin = CheckAdmin(email.data)
         if user:
-            raise ValidationError('That email is taken. Please choose another one.')
-        elif admin:
-            raise ValidationError('That email is not allowed. Please choose another one.')
+            raise ValidationError('Email already exists.')
