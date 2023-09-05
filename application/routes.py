df596d836983d59654d4c9a7fc259704e16b593a
James E. Bryer <jamesbryer098@gmail.com>
2023-09-05 14:38:00 +0100

    Added login route

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,13 @@
 from flask import render_template, request, redirect, url_for, session
 from sqlalchemy import desc
 from application import app, db, bcrypt
-from application.models import User, Movies, Comments, Genres, MovieGenres, Actors, MovieActors, Directors, MovieDirectors
-from application.forms import SignUpForm
+from application.models import User, Movies, Comments, Genres, MovieGenres, Actors, MovieActors, Directors, MovieDirectors, Cart
+from application.forms import SignUpForm, LoginForm
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html', title='Home')
+    return render_template('home.html', title='Home')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -23,3 +23,33 @@ def signup():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('/signup.html', title='Sign Up', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    message = ''
+    if request.method == 'POST':
+        print('post')
+        if form.validate_on_submit():
+            print('validated')
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                print('user found')
+                # create session variable for user_id
+                session['user_id'] = user.id
+                # check if user has a cart
+                cart = Cart.query.filter_by(user_id=user.id).first()
+                if cart:
+                    print('cart found')
+                else:
+                    print('cart not found')
+                    cart = Cart(user_id=user.id)
+                    db.session.add(cart)
+                    db.session.commit()
+                return render_template('home.html', title='Home', message="Login Successful!")
+            else:
+                print('user not found')
+                message = 'Login Unsuccessful. Please check email and password'
+        else:
+            print('not validated')
+    return render_template('/login.html', title='Login', form=form, message=message)
