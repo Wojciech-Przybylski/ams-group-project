63f9678d2e5d08b69d573596aaf1a53991d0c8c3
James E. Bryer <jamesbryer098@gmail.com>
2023-09-05 14:37:47 +0100

    Added cart basics, commented out stuff needs work

--- application/models.py
+++ application/models.py
@@ -80,6 +80,59 @@ class Bookings(db.Model):
     price = db.Column(db.Integer, nullable=False)
     showing = db.Column(db.Integer, db.ForeignKey('showings.id'), nullable=False, index=True)
 
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    
+    # def set_quantity(self, product_id, quantity):
+    #     cart_item = CartItem.query.filter_by(product_id=product_id, cart_id=self.id).first()
+    #     if cart_item:
+    #         if quantity > 0:
+    #             cart_item.quantity = quantity
+    #             db.session.commit()
+    #         else:
+    #             db.session.delete(cart_item)
+    #             db.session.commit()
+    
+    # def add_item(self, product_id):
+    #     cart_item = CartItem.query.filter_by(product_id=product_id, cart_id=self.id).first()
+    #     if cart_item:
+    #         cart_item.quantity += 1
+    #         db.session.commit()
+    #     else:
+    #         new_cart_item = CartItem(product_id=product_id, quantity=1, cart_id=self.id)
+    #         db.session.add(new_cart_item)
+    #         db.session.commit()
+    
+    # def remove_item(self, product_id):
+    #     cart_item = CartItem.query.filter_by(product_id=product_id, cart_id=self.id).first()
+    #     if cart_item:
+    #         db.session.delete(cart_item)
+    #         db.session.commit()
+
+    # def empty_cart(self):
+    #     cart_items = CartItem.query.filter_by(cart_id=self.id).all()
+    #     for item in cart_items:
+    #         db.session.delete(item)
+    #         db.session.commit()
+        
+    
+# class CartItem(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+#     quantity = db.Column(db.Integer, nullable=False)
+#     cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'), nullable=False)
+
+    
+# class CartDisplay():
+#     def __init__(self, product_id, name, price, quantity, image) -> None:
+#         self.id = product_id
+#         self.name = name
+#         self.price = price
+#         self.quantity = quantity
+#         self.image = image
+
 class BannedChars(object):
     def __init__(self, message=None):
         self.message = message
