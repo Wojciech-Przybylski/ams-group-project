2e5f293486aaa41c994646455d15d8a4e24b54ad
voy <wojciech.przybylski1@hotmail.com>
2023-09-06 12:08:27 +0100

    WP: appended app DockerFile/init.py and Jenkins...

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from os import getenv
 
 
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite"
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv("DB_PASSWORD")}@localhost:3306/flask_test"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv("DB_PASSWORD")}@localhost:3306/cinema"
 
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
