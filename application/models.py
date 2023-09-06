46489290538eea417d4e98cd72d2af7afcccc645
James E. Bryer <jamesbryer098@gmail.com>
2023-09-06 10:26:21 +0100

    removed genre foriegn key

--- application/models.py
+++ application/models.py
@@ -34,7 +34,6 @@ class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), nullable=False, unique=True, index=True)
     description = db.Column(db.String(2048), nullable=False)
-    genre = db.Column(db.Integer, db.ForeignKey('genres.id'), nullable=False, index=True)
     image = db.Column(db.String(30), nullable=False)
     release_date = db.Column(db.DateTime, nullable=False)
 
