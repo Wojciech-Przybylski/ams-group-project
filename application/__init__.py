f431df46a1ef3fba181a06ed963227cddd67156e
voy <wojciech.przybylski1@hotmail.com>
2023-09-12 12:05:09 +0100

    WP: quick update

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from os import getenv
 
 
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost:3306/cinema"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('DB_PASSWORD')}@localhost:3306/cinema"
 
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
