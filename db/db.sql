55568794592305ec4cb64fb5f401ee375e83b68a
voy <wojciech.przybylski1@hotmail.com>
2023-09-12 16:06:32 +0100

    update

--- db/db.sql
+++ db/db.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
--- Host: localhost    Database: cinema
+-- Host: mysql    Database: cinema
 -- ------------------------------------------------------
 -- Server version	8.0.33
 
