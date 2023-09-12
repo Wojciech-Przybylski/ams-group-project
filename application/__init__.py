2c197689b253ffeb81daa4600dc79b80d2759b0b
voy <wojciech.przybylski1@hotmail.com>
2023-09-12 15:13:54 +0100

    update

--- application/__init__.py
+++ application/__init__.py
@@ -10,7 +10,7 @@ from os import environ
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite"
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:password@localhost:3306/cinema"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:@localhost:3306/cinema"
 
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
